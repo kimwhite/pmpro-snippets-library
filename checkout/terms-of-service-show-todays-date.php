33f412e47cd35ef9718833ac25e47fc09a41aaf0
Kim Coleman <kim@strangerstudios.com>
2024-11-26 13:43:00 -0500

    Update terms-of-service-show-todays-date.php

--- checkout/terms-of-service-show-todays-date.php
+++ checkout/terms-of-service-show-todays-date.php
@@ -1,21 +1,29 @@
 <?php
 /**
- * Show today's date before the content of the Terms of Service page at Membership Checkout.
+ * Show today's date in the Terms of Service label at Membership Checkout.
  *
  * title: Terms Of Service Add Today's Date
  * layout: snippet
  * collection: checkout
  * category: registration-check
- * url:https://www.paidmembershipspro.com/show-todays-date-in-the-terms-of-service-box-at-membership-checkout/
+ * url: https://www.paidmembershipspro.com/show-todays-date-in-the-terms-of-service-box-at-membership-checkout/
  * 
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
+function add_todays_date_to_pmpro_tos_label( $tos_label, $tospage ) {
+    // Get today's date in the site's preferred format.
+    $todays_date = date( get_option( 'date_format' ) );
 
-function show_today_date_before_pmpro_tos_content( $pmpro_tos_content, $pmpro_tos ) {
-	$pmpro_tos_content = '<p>Today is ' . date( get_option( 'date_format' ) ) . '.</p>' . $pmpro_tos_content;
-	return $pmpro_tos_content;
+    // Modify the label to include today's date.
+    $tos_label = sprintf(
+        __( '%1$s on %2$s', 'paid-memberships-pro' ),
+        $tos_label,
+        esc_html( $todays_date )
+    );
+
+    return $tos_label;
 }
-add_filter( 'pmpro_tos_content', 'show_today_date_before_pmpro_tos_content', 10, 2 );
+add_filter( 'pmpro_tos_field_label', 'add_todays_date_to_pmpro_tos_label', 10, 2 );
