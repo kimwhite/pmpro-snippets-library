a38b64eded3014d78b95ee42cd96a8f981ff5ef0
Kim White <kim@pixelpaper.net>
2024-12-04 15:32:19 -0500

    Requires Membership Column
    These 3 snippets add a Requires Membership” Column to the admin area... Pages List, Post LIst and Category list

---
+++ admin-pages/Requires-Membership-Column-categories.php
@@ -0,0 +1,39 @@
+
+<?php
+/**
+ * Show a “Requires Membership” Column on the All Categories Screen
+ *
+ * title: Show a “Requires Membership” Column on the All Categories Screen
+ * layout: snippet
+ * collection: admin-pages
+ * category: admin, categories
+ * url: https://www.paidmembershipspro.com/show-post-page-categorys-required-membership-levels-dashboard-views/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+//add a new column to the categories view
+function requires_membership_categories_columns_head( $defaults ) {
+    $defaults['requires_membership'] = 'Requires Membership';
+    return $defaults;
+}
+//get the column data
+function requires_membership_categories_columns_content( $content, $column_name, $term_id ) {
+	if ($column_name == 'requires_membership') {
+	    global $membership_levels, $wpdb;
+		$protected_levels = array();
+		foreach($membership_levels as $level)
+		{
+				$protectedcategories = $wpdb->get_col("SELECT category_id FROM $wpdb->pmpro_memberships_categories WHERE membership_id = $level->id");
+				if( in_array( $term_id, $protectedcategories ) ) {
+				$protected_levels[] = $level->name;
+			}
+		}
+		echo implode( ', ', $protected_levels);
+	}
+}
+add_filter( 'manage_edit-category_columns', 'requires_membership_categories_columns_head' );
+add_action( 'manage_category_custom_column', 'requires_membership_categories_columns_content', 10, 3 );
---
+++ admin-pages/Requires-Membership-Column-pages.php
@@ -0,0 +1,38 @@
+
+<?php
+/**
+ * Show a “Requires Membership” Column on the All Pages Screen
+ *
+ * title: Show a “Requires Membership” Column on the All Pages Screen
+ * layout: snippet
+ * collection: admin-pages
+ * category: admin, pages
+ * url: https://www.paidmembershipspro.com/show-post-page-categorys-required-membership-levels-dashboard-views/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+//add a new column to the all pages view
+function requires_membership_pages_columns_head( $defaults ) {
+    $defaults['requires_membership'] = 'Requires Membership';
+    return $defaults;
+}
+//get the column data
+function requires_membership_pages_columns_content( $column_name, $post_ID ) {
+	if ($column_name == 'requires_membership') {
+	    global $membership_levels, $wpdb;
+		$post_levels = $wpdb->get_col("SELECT membership_id FROM {$wpdb->pmpro_memberships_pages} WHERE page_id = '{$post_ID}'");
+		$protected_levels = array();
+		foreach($membership_levels as $level) {
+			if( in_array( $level->id, $post_levels ) ) {
+				$protected_levels[] = $level->name;
+			}
+		}
+		echo implode( ', ', $protected_levels);
+	}
+}
+add_filter( 'manage_pages_columns', 'requires_membership_pages_columns_head' );
+add_action( 'manage_pages_custom_column', 'requires_membership_pages_columns_content', 10, 2 );
