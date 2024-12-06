fd8776641447c2b6cbbdff8a1103e2a3ba22892f
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2024-12-06 09:32:28 -0500

    Update stop-members-from-renewing.php

--- checkout/stop-members-from-renewing.php
+++ checkout/stop-members-from-renewing.php
@@ -6,7 +6,7 @@
  * layout: snippet
  * collection: checkout
  * category: renewals
- * link: https://www.paidmembershipspro.com/how-to-stop-members-from-renewing-their-membership-level/
+ * link: https://www.paidmembershipspro.com/prevent-membership-renewal/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
