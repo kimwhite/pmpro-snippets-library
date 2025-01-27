9d5a062d4e7345453829c285141c6e2479c470d1
Andrew Lima <andrew@arctek.co.za>
2025-01-27 16:16:27 +0200

    WPCS

--- add-ons/pmpro-addon-packages/addon-packages-level-all-access.php
+++ add-ons/pmpro-addon-packages/addon-packages-level-all-access.php
@@ -7,7 +7,7 @@
  * layout: snippet
  * collection: addons
  * category: pmpro-addon-packages
- * url: https://www.paidmembershipspro.com/allow-specific-membership-levels-to-view-addon-packages-and-require-others-to-pay/
+ * link: https://www.paidmembershipspro.com/allow-specific-membership-levels-to-view-addon-packages-and-require-others-to-pay/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -15,10 +15,9 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
-function my_pmproap_all_access_levels($levels, $user_id, $post_id)
-{
+function my_pmproap_all_access_levels( $levels, $user_id, $post_id ) {
 	//I'm just adding the level, but I could do some calculation based on the user and post id to programatically give access to content
-	$levels = array(16);	
+	$levels = array( 16 );	
 	return $levels;
 }
-add_filter("pmproap_all_access_levels", "my_pmproap_all_access_levels", 10, 3);
+add_filter( 'pmproap_all_access_levels', 'my_pmproap_all_access_levels', 10, 3 );
