5dffea1716e346cdcbe54a79ce4ba96e75f53058
Andrew Lima <andrew@arctek.co.za>
2025-01-27 16:19:49 +0200

    Update only-one-user-page-only.php

--- add-ons/pmpro-userpages/only-one-user-page-only.php
+++ add-ons/pmpro-userpages/only-one-user-page-only.php
@@ -7,7 +7,7 @@
  * layout: snippet
  * collection: add-ons, pmpro-userpages
  * category: user pages
- * url: https://www.paidmembershipspro.com/user-pages-user-page-only/
+ * link: https://www.paidmembershipspro.com/user-pages-user-page-only/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -15,13 +15,8 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
-
-
-function mypmpro_user_page_purchase_postdata( $postdata, $user, $level ){
-	
+function mypmpro_user_page_purchase_postdata( $postdata, $user, $level ) {
 	$postdata = array();
-	
 	return $postdata;
-
 }
 add_filter( 'pmpro_user_page_purchase_postdata', 'mypmpro_user_page_purchase_postdata', 10, 3 );
