6a4e5d40b1be4efd2df227a2d63a8a0a0628f204
Andrew Lima <andrew@arctek.co.za>
2025-01-27 16:30:54 +0200

    Update add-user-id-to-members-account-page.php

--- frontend-pages/add-user-id-to-members-account-page.php
+++ frontend-pages/add-user-id-to-members-account-page.php
@@ -6,24 +6,15 @@
  * layout: snippet
  * collection: frontend-pages
  * category: members-account-page
- * url: https://www.paidmembershipspro.com/show-member-id-on-the-membership-account-page-for-paid-memberships-pro/
+ * link: https://www.paidmembershipspro.com/show-member-id-on-the-membership-account-page-for-paid-memberships-pro/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-
-
-/**
- * This shows the WordPress User ID as a Member ID on the Paid Memberships Pro Account Page.
- * nothing to change.
- */
-
  function pmpro_add_user_id_account() {
 	global $current_user;
-
 	echo '<li><strong>Member ID: </strong>' . $current_user->ID . '</li>';
 }
 add_action( 'pmpro_account_bullets_top', 'pmpro_add_user_id_account' );
-
