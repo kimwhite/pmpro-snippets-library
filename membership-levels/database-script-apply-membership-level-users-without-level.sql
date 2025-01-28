2b3654c2b6518707964867ae6e8d80c0d2d7898e
Andrew Lima <andrew@arctek.co.za>
2025-01-28 08:15:12 +0200

    Update database-script-apply-membership-level-users-without-level.sql

--- membership-levels/database-script-apply-membership-level-users-without-level.sql
+++ membership-levels/database-script-apply-membership-level-users-without-level.sql
@@ -1,17 +1,18 @@
--- Give all non-member WP users level 1 with a specific start and end date set
--- change the level (1) below and start (2017-01-01) and end (2017-12-31) dates below to per your needs.
-
--- title: Database Script: Apply a Membership Level to All Users Without a Level
--- layout: snippet
--- collection: membership-levels
--- category: levels, sql
--- link: https://www.paidmembershipspro.com/database-script-apply-membership-level-users-without-level/
-
--- You can add this recipe to your site by creating a custom plugin
--- or using the Code Snippets plugin available for free in the WordPress repository.
--- Read this companion article for step-by-step directions on either method.
--- https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
-
+/**
+ * Give all non-member WP users level 1 with a specific start and end date set
+ * change the level (1) below and start (2017-01-01) and end (2017-12-31) dates below to per your needs.
+ *
+ * title: Database Script: Apply a Membership Level to All Users Without a Level
+ * layout: snippet
+ * collection: membership-levels
+ * category: levels, sql
+ * link: https://www.paidmembershipspro.com/database-script-apply-membership-level-users-without-level/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+**/
 INSERT INTO wp_pmpro_memberships_users (user_id, membership_id, status, startdate, enddate) 
 SELECT 
 	u.ID,  			#ID from wp_users table
