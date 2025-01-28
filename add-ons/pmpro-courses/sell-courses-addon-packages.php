cfa0a2d131c6e9fb36b6b25bed6698bc5f4cbaf6
Andrew Lima <andrew@arctek.co.za>
2025-01-28 07:46:40 +0200

    Update sell-courses-addon-packages.php

--- add-ons/pmpro-courses/sell-courses-addon-packages.php
+++ add-ons/pmpro-courses/sell-courses-addon-packages.php
@@ -13,31 +13,19 @@
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
- 
- 
-/**
- * Adds the AddOn Package post meta to 'Courses' post type.
- * Give access if they purchased "Courses" parent container.
- * Add this code to your PMPro Customizations Plugin - https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
- */
- 
  function add_pmpro_courses_to_AP( $types ) {
 	$types[] = 'courses';
-
 	return $types;
 }
 add_filter( 'pmproap_supported_post_types', 'add_pmpro_courses_to_AP', 10, 1 );
 
-function pmpro_give_access_to_users_for_course( $hasaccess, $mypost, $myuser, $post_membership_levels) {
-
+function pmpro_give_access_to_users_for_course( $hasaccess, $mypost, $myuser, $post_membership_levels ) {
 	$post_id = $mypost->ID;
-
 	$is_course = get_post_meta( $post_id, '_post_courses', true );
-
 	$ap_posts = get_user_meta( $myuser->ID, '_pmproap_posts', true );
 
 	// Bail if nothing is found and return current access.
-	if ( empty( $ap_posts) || empty( $is_course) ) {
+	if ( empty( $ap_posts ) || empty( $is_course ) ) {
 		return $hasaccess;
 	}
 
@@ -47,4 +35,4 @@ function pmpro_give_access_to_users_for_course( $hasaccess, $mypost, $myuser, $p
 
 	return $hasaccess;
 }
-add_filter('pmpro_has_membership_access_filter', 'pmpro_give_access_to_users_for_course', 10, 4 );
+add_filter( 'pmpro_has_membership_access_filter', 'pmpro_give_access_to_users_for_course', 10, 4 );
