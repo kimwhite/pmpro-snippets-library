d433b96ee6288f77f9ab9352f0852fb46437856c
Kim Coleman <kim@strangerstudios.com>
2025-01-30 06:03:49 -0500

    Example of how to add conditional links to the Membership Account > My Memberships section.

---
+++ frontend-pages/add-membership-level-action-links.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Example of how to add conditional links to the Membership Account > My Memberships section.
+ *
+ * title: Add Member Action links on the Membership Account Page 
+ * layout: snippet
+ * collection: frontend-pages
+ * category: membership-action-links
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function my_pmpro_member_action_links( $links, $level_id ) {
+    // Check if the member is on Level 2.
+    if ( $level_id == 2 ) {
+        // Add an Upgrade link to checkout for Level 3.
+        $links['upgrade'] = '<a id="pmpro_actionlink-upgrade" href="' . esc_url( add_query_arg( 'pmpro_level', 3, pmpro_url( 'checkout' ) ) ) . '">' . esc_html__( 'Upgrade to Pro Membership', 'my-text-domain' ) . '</a>';
+    }
+
+    // Example: Add a bonus link for Level ID 3.
+    if ( $level_id == 3 ) {
+        $links['bonuses'] = '<a id="pmpro_actionlink-bonuses" href="https://yoursite.com/bonuses">' . esc_html__( 'View Your Exclusive Bonuses', 'my-text-domain' ) . '</a>';
+    }
+
+    return $links;
+}
+add_filter( 'pmpro_member_action_links', 'my_pmpro_member_action_links', 10, 2 );
